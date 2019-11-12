0836ac1c2d58f4b65c408179d5e463d2ef654297
ohrotan <ygj02054@gmail.com>
2019-11-12 13:40:03 +0900

    코디옷추가 검색창 레이아웃, 검색결과창 레이아웃

--- app/src/main/java/com/ssu/mylook/TestRanActivity.java
+++ app/src/main/java/com/ssu/mylook/TestRanActivity.java
@@ -1,12 +1,18 @@
 package com.ssu.mylook;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +25,11 @@ public class TestRanActivity extends AppCompatActivity implements View.OnClickLi
     Button btn5;
     Button btn6;
 
+    //옷 추가 레이아웃
     AlertDialog dialog;
+    GridView listView;
+    ClotheListAdapter myListAdapter;
+    ArrayList<ClotheListItem> list_itemArrayList;
 
 
     @Override
@@ -34,23 +44,37 @@ public class TestRanActivity extends AppCompatActivity implements View.OnClickLi
         btn5 = findViewById(R.id.button5);
         btn6 = findViewById(R.id.button6);
 
+        //옷 추가 레이아웃
+        list_itemArrayList = new ArrayList<ClotheListItem>();
+        listView = (GridView)findViewById(R.id.coordi_clothe_result_view);
+
     }
 
     @Override
     public void onClick(View v) {
         Intent intent;
         if(v == btn1){ //코디할 옷 추가 레이아웃
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+           final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-            LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-            View bookAddPopup = inflater.inflate(R.layout.layout_clothe_add,null);
-            builder.setView(bookAddPopup);
-            builder.setPositiveButton("확인",new DialogInterface.OnClickListener() {
+            final LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+            View clotheAddPopup = inflater.inflate(R.layout.layout_clothe_add_search,null);
+            builder.setView(clotheAddPopup);
+            builder.setPositiveButton("다음",new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Toast.makeText(TestRanActivity.this,
-                            "코디할 옷 추가",Toast.LENGTH_SHORT).show();
-                        //제목이나 저자로 책검색 이동
+                public void onClick(DialogInterface dialo, int which) {
+                    View clotheAddPopup = inflater.inflate(R.layout.layout_clothe_add,null);
+                    builder.setView(clotheAddPopup);
+                    builder.setPositiveButton("다음",new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    builder.setNegativeButton("취소",null);
+
+
+                    dialog = builder.create();
+                    dialog.show();
 
                 }
             });
