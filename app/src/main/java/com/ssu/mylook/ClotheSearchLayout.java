a7ae5a308cdcfe658050c1c20f450875c70ca9f4
nolzaheo <nolzaheo@gmail.com>
2019-11-15 12:21:13 +0900

    ClotheSearchLayout 추가

---
+++ app/src/main/java/com/ssu/mylook/ClotheSearchLayout.java
@@ -0,0 +1,14 @@
+package com.ssu.mylook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClotheSearchLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothe_search_layout);
+    }
+}
