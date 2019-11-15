cf9c01939d1a6b406e085143fceebef46f3e4fd0
je991025 <je991025@gamil.com>
2019-11-15 19:08:28 +0900

    test

--- app/src/main/java/com/ssu/mylook/TestJeongeun.java
+++ app/src/main/java/com/ssu/mylook/TestJeongeun.java
@@ -9,6 +9,6 @@ public class TestJeongeun extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_jeongeun);
+        setContentView(R.layout.activity_test_jeongeun);//asdfasdf
     }
 }
