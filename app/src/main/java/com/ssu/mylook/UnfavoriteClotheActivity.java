85d195ef6074c893afd2549d8bfdafccd42f7161
je991025 <je991025@gamil.com>
2019-11-20 14:52:24 +0900

    성향분석 상세화면을 위한Custom View ë§만듦

--- app/src/main/java/com/ssu/mylook/UnfavoriteClotheActivity.java
+++ app/src/main/java/com/ssu/mylook/UnfavoriteClotheActivity.java
@@ -1,14 +1,19 @@
 package com.ssu.mylook;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class UnfavoriteClotheActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unfavorite_clothe);
+        ActionBar ab = getSupportActionBar() ;
+        ab.setTitle("나의 성향 분석");
     }
 }
