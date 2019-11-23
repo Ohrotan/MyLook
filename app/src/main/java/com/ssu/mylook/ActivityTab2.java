4412f07302431bb0efe10c53b77b454f3131e693
nolzaheo <nolzaheo@gmail.com>
2019-11-23 14:58:50 +0900

    하단바 액티비티 구현 중

---
+++ app/src/main/java/com/ssu/mylook/ActivityTab2.java
@@ -0,0 +1,14 @@
+package com.ssu.mylook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityTab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab2);
+    }
+}
