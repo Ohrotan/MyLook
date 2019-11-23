ff08fd2387d9128e93cd46c9cda7d490970bb48f
je991025 <je991025@gamil.com>
2019-11-23 23:29:02 +0900

    자주쓰는태그 레이아웃완성

--- app/src/main/java/com/ssu/mylook/FavoriteTagActivity.java
+++ app/src/main/java/com/ssu/mylook/FavoriteTagActivity.java
@@ -1,11 +1,15 @@
 package com.ssu.mylook;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class FavoriteTagActivity extends AppCompatActivity {
+
+    private FavoriteTagAdapter adapter;
+    private ListView MyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,28 @@ public class FavoriteTagActivity extends AppCompatActivity {
         setContentView(R.layout.activity_favorite_tag);
         ActionBar ab = getSupportActionBar() ;
         ab.setTitle("나의 성향 분석");
+
+        adapter = new FavoriteTagAdapter();
+        MyListView =(ListView)findViewById(R.id.TagListView);
+
+        setData();
+
+        MyListView.setAdapter(adapter);
+    }
+
+    private void setData() {
+        String[] ranks = getResources().getStringArray(R.array.favorite_tag_rank);
+        String[] titles = getResources().getStringArray(R.array.favorite_tag_title);
+        String[] contents = getResources().getStringArray(R.array.favorite_tag_number);
+
+        for (int i = 0; i < ranks.length; i++) {
+            CustomDTO dto = new CustomDTO();
+            dto.setRank(ranks[i]);
+            dto.setTitle(titles[i]);
+            dto.setContent(contents[i]);
+
+            adapter.addItem(dto);
+
+        }
     }
 }
