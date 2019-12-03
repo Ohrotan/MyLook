9ea77fee13670cf985285a65d7d6e70e7ce240fe
nolzaheo <nolzaheo@gmail.com>
2019-12-03 22:22:48 +0900

    버튼 수정

--- app/src/main/java/com/ssu/mylook/ClotheEditActivity.java
+++ app/src/main/java/com/ssu/mylook/ClotheEditActivity.java
@@ -2,19 +2,42 @@ package com.ssu.mylook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ClotheEditActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button save;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clothe_edit);
+        setContentView(R.layout.activity_clothe_register);
+
+        save= findViewById(R.id.cancel_btn);
+        back= findViewById(R.id.save_btn);
     }
 
     @Override
     public void onClick(View v) {
+    Intent intent;
 
+    if(v==save){
+        //저장
+        intent = new Intent(this, ClosetActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+
+    }
+    else if(v==back){
+        //이전액티비티
+        onBackPressed();
+    }
     }
 }
--- app/src/main/java/com/ssu/mylook/ClotheViewActivity.java
+++ app/src/main/java/com/ssu/mylook/ClotheViewActivity.java
@@ -2,13 +2,47 @@ package com.ssu.mylook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class ClotheViewActivity extends AppCompatActivity {
+public class ClotheViewActivity extends AppCompatActivity implements View.OnClickListener {
+    RelativeLayout edit_clothe_info;
+    RelativeLayout delete_clothe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothe_view);
+
+        edit_clothe_info=findViewById(R.id.edit_btn);
+        delete_clothe=findViewById(R.id.remove_btn);
+
+        edit_clothe_info.setOnClickListener(this);
+        delete_clothe.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        if(v==edit_clothe_info)
+        {
+            //제대로 작동안함
+            intent = new Intent(this,ClotheEditActivity.class);
+            startActivity(intent);
+        }
+        else if(v==delete_clothe)
+        {
+            //제대로 작동함
+            //이전 액티비티가 나왔으면 좋겠는데 아직 공부를 덜해서
+            intent=new Intent(this,ClosetActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            finish();//저장
+        }
     }
 }
