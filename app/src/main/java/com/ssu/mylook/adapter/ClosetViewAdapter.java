1bd50080e75e0492de5c4f5bd1c3ae0624b3386c
nolzaheo <nolzaheo@gmail.com>
2019-12-18 11:56:41 +0900

    검색기능 구현중

--- app/src/main/java/com/ssu/mylook/adapter/ClosetViewAdapter.java
+++ app/src/main/java/com/ssu/mylook/adapter/ClosetViewAdapter.java
@@ -16,6 +16,7 @@ import com.ssu.mylook.dto.ClotheItem;
 import com.ssu.mylook.util.DBUtil;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ClosetViewAdapter extends BaseAdapter {
     private ArrayList<ClotheItem> items ;
@@ -30,7 +31,7 @@ public class ClosetViewAdapter extends BaseAdapter {
     public ClosetViewAdapter(Context context, ArrayList<ClotheItem> items) {
         this.context = context;
         this.items = items;
-        items.addAll(items);
+        this.items.addAll(items);
 
     }
 
@@ -76,13 +77,30 @@ public class ClosetViewAdapter extends BaseAdapter {
         }else {
             holder = (ClosetViewAdapter.ViewHolder) convertView.getTag();
         }
+        Toast.makeText(context, ""+position, Toast.LENGTH_LONG).show();
         ClotheItem itemDTO = items.get(position);
 
 
         new DBUtil().setImageViewFromDB(context,holder.clothe_img,itemDTO.getImage());
         holder.clothe_title.setText(itemDTO.getTitle());
-        Toast.makeText(context, holder.clothe_title+ "", Toast.LENGTH_LONG).show(); //옷 이름이 안뜸
+        //Toast.makeText(context, holder.clothe_title+ "", Toast.LENGTH_LONG).show(); //옷 이름이 안뜸
 
         return convertView;
+    }
+
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        items.clear();
+        if (charText.length() == 0) {
+            items.addAll(items);
+        } else {
+            for (ClotheItem wp : items) {
+                //getTitle()에 문제가 있어서 안뜨는거같음
+                if (wp.getTitle().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    items.add(wp);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
