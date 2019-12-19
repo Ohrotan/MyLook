e54cdef2335bc6c9a9413042c010737295104286
nolzaheo <nolzaheo@gmail.com>
2019-12-19 16:03:08 +0900

    검색기능 구현

--- app/src/main/java/com/ssu/mylook/ClotheSearchActivity.java
+++ app/src/main/java/com/ssu/mylook/ClotheSearchActivity.java
@@ -42,51 +42,41 @@ public class ClotheSearchActivity extends AppCompatActivity implements View.OnCl
     EditText editText;
     ImageButton search_btn;
     String search_keyword;
+    ArrayList<ClotheDTO> list;
+    ArrayList<ClotheDTO> resultList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothe_search_layout);
 
-       editText = (EditText) findViewById(R.id.search_view);
-       search_btn = (ImageButton) findViewById(R.id.search_btn);
+        editText = (EditText) findViewById(R.id.search_view);
+        search_btn = (ImageButton) findViewById(R.id.search_btn);
 
         gridView = (GridView) findViewById(R.id.gridView);
 
         search_btn.setOnClickListener(this);
 
 
-        setData(0); //setAdapter
+        //setData(0); //setAdapter
 
-
-
-     /*   searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-// do something when the focus of the query text field changes
-
-                db.collection("clothes").whereEqualTo("title",query)
-                        .get()
-                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                            @Override
-                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                ArrayList<ClotheDTO> list = new ArrayList<>();
-                                for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
-                                    ClotheDTO item = doc.toObject(ClotheDTO.class);
-                                    item.setId(doc.getId());
-                                    list.add(item);
-                                    Toast.makeText(getApplicationContext(), ""+item.getTitle(), Toast.LENGTH_LONG).show();
-                                    //Toast.makeText(getApplicationContext(), ""+item.getTTL(), Toast.LENGTH_LONG).show();
-                                }
-
-                                closetViewAdapter = new ClosetViewAdapter(ClotheSearchActivity.this,list);
-                                gridView.setAdapter(closetViewAdapter);
-                            }});
-
-            }
-        });*/
-
-
+        //데이터를 list에 불러옴, 이걸 사용하면 됨
+        db.collection("clothes")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                         list = new ArrayList<>();
+                        for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
+                            ClotheDTO item = doc.toObject(ClotheDTO.class);
+                            item.setId(doc.getId());
+                            list.add(item);
+                            Toast.makeText(getApplicationContext(), "" + item.getTitle(), Toast.LENGTH_LONG).show();
+                            //Toast.makeText(getApplicationContext(), ""+item.getTTL(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
 
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -104,135 +94,124 @@ public class ClotheSearchActivity extends AppCompatActivity implements View.OnCl
             }
 
         });
-
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v==search_btn)
-        {
-            search_keyword=editText.getText().toString();
-            Toast.makeText(getApplicationContext(),""+search_keyword,Toast.LENGTH_LONG).show();
-            db.collection("clothes").whereEqualTo("title",search_keyword)
-                    .get()
-                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                        @Override
-                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                            ArrayList<ClotheDTO> list = new ArrayList<>();
-                            for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
-                                ClotheDTO item = doc.toObject(ClotheDTO.class);
-                                item.setId(doc.getId());
-                                list.add(item);
-                                Toast.makeText(getApplicationContext(), "" + item.getTitle(), Toast.LENGTH_LONG).show();
-                                //Toast.makeText(getApplicationContext(), ""+item.getTTL(), Toast.LENGTH_LONG).show();
+
+        @Override
+        public void onClick (View v){
+            if (v == search_btn) {
+                //검색한 단어
+                search_keyword = editText.getText().toString();
+                Toast.makeText(getApplicationContext(), "" + search_keyword, Toast.LENGTH_LONG).show();
+                resultList = new ArrayList<>();
+                //검색한 단어와 리스트의 단어를 비교
+               for(ClotheDTO dto : list){
+                   if(dto.getTitle().contains(search_keyword)==true){
+                       resultList.add(dto);
+                       //list.remove(dto);
+                   }
+               }
+                closetViewAdapter = new ClosetViewAdapter(ClotheSearchActivity.this, resultList);
+                gridView.setAdapter(closetViewAdapter);
+            }
+        }
+
+        public void clickSeason (View v){
+            TextView spring_menu = findViewById(R.id.upper_spring);
+            TextView summer_menu = findViewById(R.id.upper_summer);
+            TextView fall_menu = findViewById(R.id.upper_fall);
+            TextView winter_menu = findViewById(R.id.upper_winter);
+
+            //중복선택 되도록 짜야함
+            if (v == spring_menu) {
+                if (spring_menu.getCurrentTextColor() != Color.WHITE) {
+                    spring_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
+                    spring_menu.setTextColor(Color.WHITE);
+                } else {
+                    spring_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
+                    spring_menu.setTextColor(Color.DKGRAY);
+                }
+            }
+            if (v == summer_menu) {
+                if (summer_menu.getCurrentTextColor() != Color.WHITE) {
+                    summer_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
+                    summer_menu.setTextColor(Color.WHITE);
+                } else {
+                    summer_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
+                    summer_menu.setTextColor(Color.DKGRAY);
+                }
+            }
+            if (v == fall_menu) {
+                if (fall_menu.getCurrentTextColor() != Color.WHITE) {
+                    fall_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
+                    fall_menu.setTextColor(Color.WHITE);
+                } else {
+                    fall_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
+                    fall_menu.setTextColor(Color.DKGRAY);
+                }
+            }
+            if (v == winter_menu) {
+                if (winter_menu.getCurrentTextColor() != Color.WHITE) {
+                    winter_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
+                    winter_menu.setTextColor(Color.WHITE);
+                } else {
+                    winter_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
+                    winter_menu.setTextColor(Color.DKGRAY);
+                }
+            }
+
+        }
+
+        public void clickTab (View v){
+            ImageView[] img = new ImageView[3];
+            img[0] = findViewById(R.id.tab_closet);
+            img[1] = findViewById(R.id.tab_coordi);
+            img[2] = findViewById(R.id.tab_analysis);
+
+            if (v == img[0]) {
+                Intent intent = new Intent(this, ClosetActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            } else if (v == img[1]) {
+                Intent intent = new Intent(this, CoordiMainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            } else if (v == img[2]) {
+                Intent intent = new Intent(this, StyleAnalysisActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+
+
+        }
+
+
+     /*   private void setData ( int position){
+
+            if (position == 0) {
+                db.collection("clothes").orderBy("regdate", Query.Direction.DESCENDING)
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                ArrayList<ClotheDTO> list = new ArrayList<>();
+                                for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
+                                    ClotheDTO item = doc.toObject(ClotheDTO.class);
+                                    item.setId(doc.getId());
+                                    list.add(item);
+                                    //clotheNameList[k]+=item.getTTL().toString();
+                                    // Toast.makeText(getApplicationContext(),"옷 이름: "+clotheNameList[k].toString(),Toast.LENGTH_LONG);
+                                }
+                                closetViewAdapter = new ClosetViewAdapter(ClotheSearchActivity.this, list);
+                                gridView.setAdapter(closetViewAdapter);
                             }
+                        });
 
-                            closetViewAdapter = new ClosetViewAdapter(ClotheSearchActivity.this, list);
-                            gridView.setAdapter(closetViewAdapter);
-                        }
-                    });
-        }
-    }
-
-    public void clickSeason(View v) {
-        TextView spring_menu = findViewById(R.id.upper_spring);
-        TextView summer_menu = findViewById(R.id.upper_summer);
-        TextView fall_menu = findViewById(R.id.upper_fall);
-        TextView winter_menu = findViewById(R.id.upper_winter);
-
-        //중복선택 되도록 짜야함
-        if (v == spring_menu) {
-            if (spring_menu.getCurrentTextColor() != Color.WHITE) {
-                spring_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
-                spring_menu.setTextColor(Color.WHITE);
-            } else {
-                spring_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
-                spring_menu.setTextColor(Color.DKGRAY);
             }
-        }
-        if (v == summer_menu) {
-            if (summer_menu.getCurrentTextColor() != Color.WHITE) {
-                summer_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
-                summer_menu.setTextColor(Color.WHITE);
-            } else {
-                summer_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
-                summer_menu.setTextColor(Color.DKGRAY);
-            }
-        }
-        if (v == fall_menu) {
-            if (fall_menu.getCurrentTextColor() != Color.WHITE) {
-                fall_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
-                fall_menu.setTextColor(Color.WHITE);
-            } else {
-                fall_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
-                fall_menu.setTextColor(Color.DKGRAY);
-            }
-        }
-        if (v == winter_menu) {
-            if (winter_menu.getCurrentTextColor() != Color.WHITE) {
-                winter_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonClicked, null));
-                winter_menu.setTextColor(Color.WHITE);
-            } else {
-                winter_menu.setBackground(getResources().getDrawable(R.drawable.colorButtonNotClick, null));
-                winter_menu.setTextColor(Color.DKGRAY);
-            }
-        }
 
-    }
+        }*/
 
-    public void clickTab(View v) {
-        ImageView[] img = new ImageView[3];
-        img[0] = findViewById(R.id.tab_closet);
-        img[1] = findViewById(R.id.tab_coordi);
-        img[2] = findViewById(R.id.tab_analysis);
-
-        if (v == img[0]) {
-            Intent intent = new Intent(this, ClosetActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            startActivity(intent);
-            overridePendingTransition(0, 0);
-        } else if (v == img[1]) {
-            Intent intent = new Intent(this, CoordiMainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            startActivity(intent);
-            overridePendingTransition(0, 0);
-        } else if (v == img[2]) {
-            Intent intent = new Intent(this, StyleAnalysisActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            startActivity(intent);
-            overridePendingTransition(0, 0);
-        }
-
-
-    }
-
-
-
-
-
-
-    private void setData(int position) {
-
-        if (position == 0) {
-            db.collection("clothes").orderBy("regdate", Query.Direction.DESCENDING)
-                    .get()
-                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                        @Override
-                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                            ArrayList<ClotheDTO> list = new ArrayList<>();
-                            for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
-                                ClotheDTO item = doc.toObject(ClotheDTO.class);
-                                item.setId(doc.getId());
-                                list.add(item);
-                                //clotheNameList[k]+=item.getTTL().toString();
-                                // Toast.makeText(getApplicationContext(),"옷 이름: "+clotheNameList[k].toString(),Toast.LENGTH_LONG);
-                            }
-                            closetViewAdapter = new ClosetViewAdapter(ClotheSearchActivity.this, list);
-                            gridView.setAdapter(closetViewAdapter);
-                        }
-                    });
-
-        }
-
-    }
 }
